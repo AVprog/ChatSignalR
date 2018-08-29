0686a43377348866d54336e86888f3c2aba38ed8
Aliaksandr <sherbakovu@mail.ru>
2018-08-29 12:14:11 +0300

    client code work

--- WebApplication23/ClientApp/src/app/signal/signal.component.ts
+++ WebApplication23/ClientApp/src/app/signal/signal.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as signalR from "@aspnet/signalr";
 
 @Component({
   selector: 'app-signal',
@@ -7,9 +8,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignalComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+    this.connection = new signalR.HubConnectionBuilder().withUrl("/chatHub").build();
+   }
+
+  public connection:signalR.HubConnection;
+  public masMsg:string[]=[];
 
   ngOnInit() {
+    
+
+    this.connection.start();
+
+    this.connection.on("ReceiveMessage",  (user, message) => {      
+        this.masMsg.push(`${user} ${message}`);
+        
+    });
+
+
   }
+
+  sendMessage(user:string,message:string)
+  {
+    this.connection.invoke("SendMessage", user, message);
+  }
+
+
+  
 
 }
